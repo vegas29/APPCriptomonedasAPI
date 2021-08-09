84f6f12f4b8ecdd546b4467c18fcb1396e1d4580
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2021-08-09 15:41:32 -0500

    Llenando el select con las criptomonedas

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const criptomonedasSelect = document.querySelector('#criptomonedas');
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    consultarCriptomonedas();
+});
+
+//Crear un promise
+
+const obtenerCriptomonedas = criptomonedas => new Promise(resolve =>{
+    resolve(criptomonedas);
+});
+
+function consultarCriptomonedas(){
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=COP';
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => obtenerCriptomonedas(resultado.Data))
+        .then( criptomonedas => selectCriptomonedas(criptomonedas));
+}
+
+function selectCriptomonedas(criptomonedas){
+    criptomonedas.forEach(cripto =>{
+        const {FullName, Name} = cripto.CoinInfo;
+
+        const option = document.createElement('option');
+        option.value = Name;
+        option.textContent = FullName;
+        criptomonedasSelect.appendChild(option);
+    })
+}
